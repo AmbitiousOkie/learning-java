43f17cc2f1be341961c7d2f9a09e862f829ad9b2
Kris Wall <kris@urbanscouter.com>
2017-12-12 12:57:28 -0600

    input and output of doubles

--- ch2/BasicInputOutput.java
+++ ch2/BasicInputOutput.java
@@ -7,6 +7,14 @@ class BasicInputOutput {
        System.out.print("Please enter the number of bottles: ");
        int bottles = in.nextInt();
 
-       System.out.println("That's a total of: "+ bottles + " bottles!");
+       System.out.println("That's a total of "+ bottles + " bottles!");
+
+
+       System.out.print("Please enter the total amount for the bottles: ");
+       double amount = in.nextDouble();
+
+       System.out.printf("That's a total of %.2f", amount);
+       System.out.println();
+
     }
  } 
